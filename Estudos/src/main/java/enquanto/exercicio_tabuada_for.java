de04b18d3779221601d0ffb63253db0d9e532f8c
guialvesds <guilherme_elaves@outlook.com>
2026-03-03 21:08:34 -0300

    Exercicio Tabuada

---
+++ Estudos/src/main/java/enquanto/exercicio_tabuada_for.java
@@ -0,0 +1,20 @@
+package enquanto;
+
+import java.util.Scanner;
+
+public class exercicio_tabuada_for {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i=0; i<=10; i++){
+            int produto = i * N;
+            System.out.println(N + " x " + i + " = " + produto);
+        }
+
+        sc.close();
+
+    }
+}
