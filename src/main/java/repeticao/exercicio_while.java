b5732eebb17a097ee3cb0879c0196232db7b8112
guialvesds <guilherme_ealves@outlook.com>
2026-03-03 21:35:48 -0300

    comentario

--- src/main/java/repeticao/exercicio_while.java
+++ src/main/java/repeticao/exercicio_while.java
@@ -9,6 +9,7 @@ public class exercicio_while {
         int x = sc.nextInt();
         int y = sc.nextInt();
 
+        // O loop while é uma estrutura de controle de fluxo que permite repetir um bloco de código enquanto uma condição específica for verdadeira. A sintaxe básica do loop while é a seguinte:
         while(x != y) {
             if(x > y){
                 System.out.println("Decrecente");
