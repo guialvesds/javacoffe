e012baafd8bb4436dcbaefa1115a97319764e634
guialvesds <guilherme_elaves@outlook.com>
2026-03-03 21:01:21 -0300

    Estrutura for

---
+++ Estudos/src/main/java/enquanto/exercicio_for.java
@@ -0,0 +1,22 @@
+package enquanto;
+
+import java.util.Scanner;
+
+public class exercicio_for {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int soma = 0;
+
+        for(int i = 0; i < N; i++){
+            int x = sc.nextInt();
+            soma += x;
+        }
+
+        System.out.println(soma);
+
+        sc.close();
+    }
+}
