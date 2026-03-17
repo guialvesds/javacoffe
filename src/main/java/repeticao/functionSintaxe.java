c819f99c3ea40d66c338dbcf9e99341ee4271fc4
guialvesds <guilherme_ealves@outlook.com>
2026-03-16 21:22:15 -0300

    Function example

--- src/main/java/repeticao/functionSintaxe.java
+++ src/main/java/repeticao/functionSintaxe.java
@@ -1,7 +1,42 @@
 package repeticao;
 
+import java.util.Scanner;
+
+import static sun.swing.MenuItemLayoutHelper.max;
+
 public class functionSintaxe {
     public static void main(String[] args){
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int higher = max(a, b, c);
+
+        showResult(higher);
+
+        sc.close();
+
     }
+
+    public static int max(int x, int y, int z) {
+        int aux;
+
+        if(x > y && x > z) {
+            aux = x;
+        } else if (y > z) {
+            aux = y;
+        } else {
+            aux = z;
+        }
+        return aux;
+    }
+
+    public static void showResult(int value){
+        System.out.print("Valor maior é: " + value);
+    }
+
 }
